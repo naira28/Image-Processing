6d6bc208a8af8523eef5e9a8ac79fa9593fb2a4d
naira28 <111312265+naira28@users.noreply.github.com>
2022-10-23 19:58:39 +0200

    Add files via upload

---
+++ rgb_to_gray.m
@@ -0,0 +1,22 @@
+
+function [gray]=rgb_to_gray(rgb,option)
+[h ,w, l]=size(rgb);
+gray=zeros(h,w);
+gray=double(gray);
+for i=1 :h
+    for j=1:w
+        if option==1
+        gray(i,j)=(rgb(i,j,1)+rgb(i,j,2)+rgb(i,j,3))/3;
+        end
+        if option==2
+             gray(i,j)=(rgb(i,j,1)*0.6 +rgb(i,j,2)*0.1+rgb(i,j,3)*0.3);
+        end
+        if option==3
+            gray(i,j)=rgb(i,j,l);
+        end
+    end
+end
+gray= uint8(gray);
+imshow(gray);
+end
+
