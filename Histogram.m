d2417bad2ec09cddff513e12dd266f4686d1dd4e
naira28 <111312265+naira28@users.noreply.github.com>
2022-11-03 00:21:37 +0200

    Update Histogram.m

--- Histogram.m
+++ Histogram.m
@@ -3,7 +3,15 @@ arrhist=zeros(256,1);
 [w, h, l]=size(img);
 for i=1:w
     for j=1:h
-        arrhist(img(i,j)+1)=arrhist(img(i,j)+1)+1;
+     if l==3
+            arrhist(img(i,j,1)+1)=arrhist(img(i,j,1)+1)+1;
+            arrhist(img(i,j,2)+1)=arrhist(img(i,j,2)+1)+1;
+            arrhist(img(i,j,3)+1)=arrhist(img(i,j,3)+1)+1;
+        
+        else
+         arrhist(img(i,j)+1)=arrhist(img(i,j)+1)+1;
+     end
+        
     end
 end
 
