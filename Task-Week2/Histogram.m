908b5dc841e1f318f5bdd2db425d1e49ba74150c
Yousef360 <95775013+YousefTB@users.noreply.github.com>
2022-11-05 01:28:07 +0200

    Add files via upload

---
+++ Task-Week2/Histogram.m
@@ -0,0 +1,14 @@
+function [] = Histogram(img)
+arrhist=zeros(256,1);
+[w, h]=size(img);
+for i=1:w
+    for j=1:h
+        
+         arrhist(img(i,j)+1)=arrhist(img(i,j)+1)+1;
+       
+    end
+end
+
+bar(arrhist);
+end
+
